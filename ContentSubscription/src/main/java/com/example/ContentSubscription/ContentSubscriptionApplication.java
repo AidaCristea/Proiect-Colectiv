9fb6ad4b6595d24661ddd5cc6a626f9a557b7922
912-Cristea-Aida <aida.cristea@stud.ubbcluj.ro>
2023-11-15 12:32:27 +0200

    Created the project

---
+++ ContentSubscription/src/main/java/com/example/ContentSubscription/ContentSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.ContentSubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentSubscriptionApplication.class, args);
+	}
+
+}
---
+++ ContentSubscription/src/test/java/com/example/ContentSubscription/ContentSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ContentSubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentSubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
