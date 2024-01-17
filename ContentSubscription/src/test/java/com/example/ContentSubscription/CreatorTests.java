f6e6537fe145c79251177f0148469a2bb5b5cf32
912-Cristea-Aida <aida.cristea@stud.ubbcluj.ro>
2024-01-17 18:39:00 +0200

    creator add test

--- ContentSubscription/src/test/java/com/example/ContentSubscription/CreatorTests.java
+++ ContentSubscription/src/test/java/com/example/ContentSubscription/CreatorTests.java
@@ -1,47 +1,93 @@
 package com.example.ContentSubscription;
 
+import com.example.ContentSubscription.controller.CreatorController;
+import com.example.ContentSubscription.converter.CreatorConverter;
 import com.example.ContentSubscription.domain.Creator;
+import com.example.ContentSubscription.dtos.CreatorDto;
 import com.example.ContentSubscription.repository.CreatorRepo;
 import com.example.ContentSubscription.service.CreatorService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.web.client.HttpServerErrorException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.ExpectedCount.times;
 
 
-@DataJpaTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // Reset database after each test
+@ExtendWith(MockitoExtension.class)
 public class CreatorTests {
 
-    @Autowired
-    private TestEntityManager entityManager;
+    private List<Creator> creators = new ArrayList<>();
 
-    @Autowired
-    private CreatorRepo creatorRepo;
+    @Mock
+    private CreatorConverter creatorConverter;
 
-    @Autowired
+    @Mock
     private CreatorService creatorService;
 
+    @InjectMocks
+    private CreatorController creatorController;
+
     @Test
-    public void testCreateAndRetrieveCreator() {
-        Creator creator = new Creator();
-        creator.setBio("Sample Bio");
-        creator.setPriceUltimate(100L);
-        creator.setPriceLite(50L);
-        creator.setPricePro(75L);
+    public void testCreateCreator() {
 
-        creator = entityManager.persistAndFlush(creator);
+        Creator creatorToAdd = Creator.builder()
+                .creatorId(1L)
+                .bio("Sample Bio")
+                .priceUltimate(100L)
+                .priceLite(50L)
+                .pricePro(75L)
+                .photoURL("url")
+                .subscriptionTypes(new ArrayList<>())
+                .build();
 
-        assertNotNull(creator.getCreatorId());
 
-        Creator retrievedCreator = creatorService.getCreatorById(creator.getCreatorId());
-        assertNotNull(retrievedCreator);
-        assertEquals("Sample Bio", retrievedCreator.getBio());
+        Creator addedCreator = Creator.builder()
+                .creatorId(1L)
+                .bio("Sample Bio")
+                .priceUltimate(100L)
+                .priceLite(50L)
+                .pricePro(75L)
+                .photoURL("url")
+                .subscriptionTypes(new ArrayList<>())
+                .build();
+
+        CreatorDto creatorDtoToAdd = CreatorDto.builder()
+                .bio("Sample Bio")
+                .priceUltimate(100L)
+                .priceLite(50L)
+                .pricePro(75L)
+                .photoURL("url")
+                .build();
+
+
+        when(creatorService.createCreator(creatorToAdd)).thenReturn(addedCreator);
+        when(creatorConverter.convertDtoToEntity(creatorDtoToAdd)).thenReturn(creatorToAdd);
+
+        ResponseEntity<CreatorDto> response = creatorController.addCreator(creatorDtoToAdd);
+        verify(creatorConverter, Mockito.times(1)).convertDtoToEntity(creatorDtoToAdd);
+        verify(creatorService, Mockito.times(1)).createCreator(creatorToAdd);
+
+        assertAll(
+                () -> assertEquals(HttpStatus.CREATED, response.getStatusCode(),
+                        "Actual status code and expected code are not the same!")
+        );
     }
 
 
